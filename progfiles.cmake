1c80728aa39ad9e2da0918aecb2aef3aa8cb5e24
Johannes Ruehle <johannes.ruehle@uni-jena.de>
2013-11-12 14:17:35 +0100

    add CMake build system

---
+++ progfiles.cmake
@@ -0,0 +1,8 @@
+# find . -name "*.cpp" -ipath "*/progs/*" > progfiles.cmake.t
+set(nice_segmentation_PROGFILES_SRC 
+./progs/toyExample.cpp
+./progs/completeEvaluationFastMinkernel.cpp
+)
+
+set(nice_segmentation_PROGFILES_HDR
+)
