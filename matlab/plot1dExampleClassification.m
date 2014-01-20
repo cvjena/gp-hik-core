96d161ae63f9401401ab3ca4fd2e237511192f4a
Alexander Freytag <alexander.freytag@uni-jena.de>
2014-01-20 10:36:37 +0100

    Matlab visualizations with smarter layout

--- matlab/plot1dExampleClassification.m
+++ matlab/plot1dExampleClassification.m
@@ -1,14 +1,19 @@
-myData = [ 0.2; 0.8];
+% BRIEF: Small visualization script using GPHIKClassifier
+% author: Alexander Freytag
+% date: 20-01-2014 (dd-mm-yyyy)
+
+myData = [ 0.2; 0.6; 0.9];
 % create l1-normalized 'histograms'
-myData = cat(2,myData , 1-myData)';
-myLabels = [1,2];
+myData = cat(2,myData , 1-myData);
+myLabels = [1; 2; 2];
 
 
 % init new GPHIKClassifier object
 myGPHIKClassifier = GPHIKClassifier ( 'verbose', 'false', ...
     'optimization_method', 'none', 'varianceApproximation', 'approximate_fine',...
     'nrOfEigenvaluesToConsiderForVarApprox',2,...
-    'uncertaintyPredictionForClassification', true ...
+    'uncertaintyPredictionForClassification', true, ...
+    'noise', 0.000001 ...
     );
 
 % run train method
@@ -28,8 +33,10 @@ for i=1:size(myDataTest,1)
 end
 
 
+% create figure and set title
+classificationFig = figure;
+set ( classificationFig, 'name', 'Classification with GPHIK');
 
-figure;
 hold on;
 
 %#initialize x array
@@ -48,7 +55,33 @@ Y=[uncLower',fliplr(uncUpper')];
 %#plot filled area
 fill(X,Y,'y');                  
 
-plot ( x,scores,'rx');
+% plot mean values
+plot ( x,scores, ...
+       'LineStyle', '--', ...
+       'LineWidth', 2, ...
+       'Color', 'r', ...
+       'Marker','none', ...
+       'MarkerSize',1, ...
+       'MarkerEdgeColor','r', ...
+       'MarkerFaceColor',[0.5,0.5,0.5] ...
+       );
+
+% plot training data
+plot ( myData(:,1), 2*(myLabels==1)-1, ...
+       'LineStyle', 'none', ...
+       'LineWidth', 3, ...
+       'Marker','o', ...
+       'MarkerSize',6, ...
+       'MarkerEdgeColor','b', ...
+       'MarkerFaceColor',[0.5,0.5,0.5] ...
+       );
+
+xlabel('1st Input dimension');
+ylabel('Classification score');   
+
+i_fontSizeAxis = 16;
+set(get(gca,'XLabel'), 'FontSize', i_fontSizeAxis);
+set(get(gca,'YLabel'), 'FontSize', i_fontSizeAxis);
 
 
 % clean up and delete object
