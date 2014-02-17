cd79ff34926e01063f15e905322af076593f0c50
Johannes Ruehle <johannes.ruehle@uni-jena.de>
2014-02-17 13:14:04 +0100

    cmake: added support for recursive source code scan; excluding matlab/* from build

---
+++ list_exclude_from_build.cmake
@@ -0,0 +1,9 @@
+SET(list_exclude_from_build_SRC
+matlab/ConverterMatlabToNICE.cpp
+matlab/ConverterMatlabToNICE.h
+matlab/ConverterNICEToMatlab.cpp
+matlab/ConverterNICEToMatlab.h
+matlab/GPHIKClassifierMex.cpp
+matlab/GPHIKRegressionMex.cpp	
+)
+
